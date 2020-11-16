b7249748a05c83d787f7eea33afcf7f854b3cd0b
mart vettner <martvettner@gmail.com>
2020-11-16 15:00:49 +0200

    Add project files.

---
+++ FileProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\martv\Desktop\New folder";
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+
+            List<string> lines = new List<string>();
+
+
+            foreach(string file in files)
+            {
+                Console.WriteLine(file);
+                var fileData = new FileInfo(file);
+                Console.WriteLine(fileData);
+
+                string fileName = fileData.Name;
+                string FileDirectory = fileData.DirectoryName;
+                long fileSize = fileData.Length;
+
+                Console.WriteLine($"File name: {fileName}; location: {FileDirectory}; size: {fileSize}");
+                string line = $"File name: {fileName}; location: {FileDirectory}; size: {fileSize} bytes";
+
+                lines.Add(line);
+
+            }
+
+            string fileDataPath = @"C:\Users\martv\Desktop\New folder\FileData.txt";
+            File.WriteAllLines(fileDataPath, lines);
+        }
+    }
+}
